7e75b7e8d93f4baa57c2120f435e60fe40441982
Serhii Volokitin <volokitin14sv@gmail.com>
2022-11-21 17:40:35 +0200

    large update

--- oTherSide/server.js
+++ oTherSide/server.js
@@ -1,15 +1,32 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {                                      //server create
     console.log('Server request');
     console.log(req.url, req.method);
+    
+    if (req.url = '/') {
+        fs.readFile('./inc/Index.html', (err, data) => {
+            if (err) {
+                console.log(err);
+                res.end();
+            }
+            else {
+                res.write(data);
+                res.end();
+            }
+        });
+    }
 
+
+/*   
     res.setHeader('Content-Type', 'text/html');                                       //response to client
     res.write('<head><link rel="stylesheet" href="#"></head>');
     res.write('<h1>Hello pidar</h1>');
     res.write('<p>Hello, my name is...</p>');                                                         
-    res.end();
+    res.end();*/
 });
 
 server.listen(3000, 'localhost', (error) => {                                         //start server
